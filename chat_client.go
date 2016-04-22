dd5d20b53a23b3f2b4265b17915ac0ef292a0b9d
zeffee <me@zeffee.com>
2016-04-22 18:42:16 +0800

    First
    多人聊天

---
+++ chat_client.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+type Conn struct {
+	conn net.Conn
+}
+
+func main() {
+	//获取IP
+	ip := Input_Ip()
+	//连接服务器
+	conn := Connect(ip + ":9988")
+	//begin
+	go conn.Receive()
+	conn.Chat()
+}
+
+//输入服务器IP
+func Input_Ip() string {
+	fmt.Println("Please input server IP .")
+	line, _, _ := bufio.NewReader(os.Stdin).ReadLine()
+	return string(line)
+}
+
+//检查错误
+func Check_err(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+//连接服务器
+func Connect(ip string) *Conn {
+	//30s则连接超时
+	c, err := net.DialTimeout("tcp", ip, 30*time.Second)
+	Check_err(err)
+	return &Conn{c}
+}
+
+//输入以及发送信息
+func (c *Conn) Chat() {
+	var Reader = func(mode bool) {
+		reader := bufio.NewReader(os.Stdin)
+		line, _, err := reader.ReadLine()
+		Check_err(err)
+		if mode {
+			//加密后发送
+			c.conn.Write(Encypt(line, []byte("www.zeffee.com")))
+		} else {
+			c.conn.Write(line)
+		}
+	}
+	fmt.Println("Who are u?")
+	Reader(false)
+	fmt.Println("Welcome!")
+	for {
+		Reader(true)
+	}
+}
+
+//接收信息
+func (c *Conn) Receive() {
+	for {
+		bytes := make([]byte, 256)
+		_, err := c.conn.Read(bytes)
+		Check_err(err)
+		//解密之后输出,不解密前16个字符
+		fmt.Println(string(bytes[:16]) + Decrypt(bytes[16:], []byte("www.zeffee.com")))
+	}
+}
+
+//加密信息
+func Encypt(source, the_key []byte) (mcrypt []byte) {
+	today := time.Now().Day()
+	//添加文本总长度
+	mcrypt = append(mcrypt, byte(len(source)))
+	for i, val := range source {
+		if i%2 == 0 {
+			mcrypt = append(mcrypt, val^byte(today))
+		} else {
+			mcrypt = append(mcrypt, val^the_key[i%len(the_key)])
+		}
+	}
+
+	return mcrypt
+}
+
+//解密信息
+func Decrypt(mcrypt, the_key []byte) (source string) {
+	today := time.Now().Day()
+	//获取文本总长度
+	count := int(mcrypt[0])
+	for i, val := range mcrypt[1:] {
+		if i >= count {
+			break
+		}
+		if i%2 == 0 {
+			source += string(val ^ byte(today))
+		} else {
+			source += string(val ^ the_key[i%len(the_key)])
+		}
+	}
+	return source
+}
---
+++ chat_server.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+type Server struct {
+	Chaters map[net.Conn]string
+}
+
+func main() {
+	//设置监听端口
+	port := "9988"
+	Start_server(port)
+}
+
+/** 开启服务
+*	@params    port  		   string	监听端口
+*
+**/
+func Start_server(port string) {
+	//初始化
+	chaters := Server{make(map[net.Conn]string, 0)}
+
+	//监听
+	conn, err := net.Listen("tcp", "localhost:"+port)
+	chaters.Check_err(err, nil)
+	fmt.Println("Start Ok!")
+	for {
+		//允许握手
+		c, err := conn.Accept()
+		chaters.Check_err(err, nil)
+
+		//添加线程
+		go chaters.Server_chat(c)
+	}
+}
+
+/** 检查错误
+*	@params    err			   error
+*	@params    conn  		   net.Conn  	聊天线程
+*
+*	@return    string 		   "break"   	判断有没人退出
+**/
+func (server *Server) Check_err(err error, conn net.Conn) string {
+	if err != nil {
+		if conn != nil {
+			//有人退出
+			return "break"
+		} else {
+			//error
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+	}
+	return ""
+}
+
+/** 监听多线程聊天
+*
+*	@params    conn  		   net.Conn  	聊天线程
+**/
+func (server *Server) Server_chat(conn net.Conn) {
+	//第一次加入
+	server.Chat(true, conn)
+
+	for {
+		//重复监听，判断是否有人退出
+		if server.Chat(false, conn) == "break" {
+			break
+		}
+	}
+}
+
+/** 多线程聊天
+*	@params    bool   mode   		(true为第一次加入)
+*	@params    conn   net.Conn  	聊天线程
+*
+*	@return    string "break"   判断有没人退出
+**/
+func (server *Server) Chat(mode bool, conn net.Conn) string {
+	//接收信息
+	bytes, err := server.Receive(mode, conn)
+
+	//有人退出
+	if server.Check_err(err, conn) == "break" {
+		//broadcast
+		server.Broadcast([]byte(server.Chaters[conn]+" OUT!"), conn)
+		//delete the map
+		delete(server.Chaters, conn)
+		conn.Close()
+		return "break"
+	}
+
+	//广播
+	broadcast_text := server.Handle_message(mode, bytes, conn)
+	server.Broadcast(broadcast_text, conn)
+	return ""
+}
+
+/** 整理广播信息
+*	@params    mode   		bool   		(true为第一次加入)
+*	@params    []byte   	bytes   	服务器接收的信息
+*	@params    conn  		net.Conn  	聊天线程
+*
+*	@return    []byte
+**/
+func (server *Server) Handle_message(mode bool, bytes []byte, conn net.Conn) (broadcast_text []byte) {
+	if mode == true {
+		//第一次加入
+		broadcast_text = []byte(string(bytes) + " IN !")
+		//add to map
+		server.Chaters[conn] = string(bytes)
+	} else {
+		//say something
+		broadcast_text = append([]byte(server.Chaters[conn]+" Say: "), bytes...)
+	}
+	return
+}
+
+/** 接收信息
+*	@params    mode   	bool   		(true为第一次加入)
+*	@params    conn  	net.Conn  	聊天线程
+*
+*	@return    []byte && error
+**/
+func (server *Server) Receive(mode bool, conn net.Conn) (bytes []byte, err error) {
+	if mode == true {
+		//姓名10字节
+		bytes = make([]byte, 10)
+	} else {
+		//内容256字节
+		bytes = make([]byte, 256)
+	}
+
+	//接收信息
+	_, err = conn.Read(bytes)
+	return
+}
+
+/** 广播
+*	@params    broadcast_text  []byte  		广播内容
+*	@params    conn  		   net.Conn  	聊天线程
+*
+**/
+func (server *Server) Broadcast(broadcast_text []byte, conn net.Conn) {
+	if len(broadcast_text) == 0 {
+		return
+	}
+	for key, _ := range server.Chaters {
+		//跳过自己
+		if key == conn {
+			continue
+		}
+		_, err := key.Write(broadcast_text)
+		server.Check_err(err, nil)
+	}
+}
